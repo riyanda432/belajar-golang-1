27596e59a7b43c614476ca6acde59d4fe9ca1b1e
riyanda432 <febry.priyangga@gmail.com>
2024-05-09 09:13:25 +0700

    test folder client

---
+++ greet/client/greet.go
@@ -0,0 +1,18 @@
+package greet
+
+import (
+	"context"
+	"log"
+
+)
+
+func doGreet(c pb.GreetServiceClient) {
+	log.Println("doGreet was invoked")
+	r, err := c.Greet(context.Background(), &pb.GreetRequest{FirstName: "Clement"})
+
+	if err != nil {
+		log.Fatalf("Could not greet: %v\n", err)
+	}
+
+	log.Printf("Greeting: %s\n", r.Result)
+}
